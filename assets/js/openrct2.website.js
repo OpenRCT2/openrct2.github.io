daa0210729d2043fb378b2b5eece3b3850d5d53b
Michael Steenbeek <m.o.steenbeek@gmail.com>
2021-07-19 13:27:27 +0200

    Update for v0.3.4 and offer more user-friendly builds by default

--- assets/js/openrct2.website.js
+++ assets/js/openrct2.website.js
@@ -4,27 +4,27 @@ openrct2.Platform = Object.freeze({
     UNKNOWN: {},
     WINDOWS32: {
         name: 'Windows (32-bit)',
-        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.3/OpenRCT2-0.3.3-windows-portable-win32.zip',
-        size: 15987866,
-        version: '0.3.3'
+        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.4/OpenRCT2-0.3.4-windows-installer-win32.exe',
+        size: 15836275,
+        version: '0.3.4'
     },
     WINDOWS64: {
         name: 'Windows (64-bit)',
-        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.3/OpenRCT2-0.3.3-windows-portable-x64.zip',
-        size: 17073701,
-        version: '0.3.3'
+        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.4/OpenRCT2-0.3.4-windows-installer-x64.exe',
+        size: 17128354,
+        version: '0.3.4'
     },
     MACOS: {
         name: 'macOS',
-        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.3/OpenRCT2-0.3.3-macos-x86-64.zip',
-        size: 26987669,
-        version: '0.3.3'
+        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.4/OpenRCT2-0.3.4-macos-x86-64.zip',
+        size: 29864542,
+        version: '0.3.4'
     },
     LINUX: {
         name: 'Linux',
-        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.3/OpenRCT2-0.3.3-linux-x86_64.tar.gz',
-        size: 59421592,
-        version: '0.3.3'
+        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.3.4/OpenRCT2-0.3.4-linux-x86_64.AppImage',
+        size: 41051328,
+        version: '0.3.4'
     }
 });  // Object.freeze() prevents this from being futzed with
 
