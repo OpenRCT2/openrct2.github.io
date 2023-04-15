933733e2c54a70b390c79a67c72faf631da87aa7
Steven Yeh <Steven.Yeh@coxautoinc.com>
2023-04-14 21:24:17 -0500

    Modernize the JS

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,18 +1,21 @@
-// http://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
-function formatBytes (bytes, decimals) {
-   if(bytes == 0) return '0 Byte';
-   var k = 1000;
-   var dm = decimals + 1 || 3;
-   var sizes = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-   var i = Math.floor(Math.log(bytes) / Math.log(k));
+// https://stackoverflow.com/questions/15900485/correct-way-to-convert-size-in-bytes-to-kb-mb-gb-in-javascript
+const formatBytes = (bytes, decimals) => {
+   if (bytes == 0) {
+    return '0 Byte';
+   }
+   const k = 1000;
+   const dm = decimals + 1 || 3;
+   const sizes = ['bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+   const i = Math.floor(Math.log(bytes) / Math.log(k));
+
    return (bytes / Math.pow(k, i)).toPrecision(dm) + ' ' + sizes[i];
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  var link = document.getElementsByClassName('link')[0];
-  var size = document.getElementsByClassName('size')[0];
-  var version = document.getElementsByClassName('version')[0];
-  var platform = document.getElementsByClassName('platform')[0];
+document.addEventListener('DOMContentLoaded', () => {
+  const link = document.getElementsByClassName('link')[0];
+  const size = document.getElementsByClassName('size')[0];
+  const version = document.getElementsByClassName('version')[0];
+  const platform = document.getElementsByClassName('platform')[0];
 
   if (openrct2.currentPlatform !== openrct2.Platform.UNKNOWN) {
     link.href = openrct2.currentPlatform.link;
@@ -21,12 +24,12 @@ document.addEventListener('DOMContentLoaded', function () {
     version.innerHTML = openrct2.currentPlatform.version;
   }
 
-  platform.addEventListener('click', function (e) {
+  platform.addEventListener('click', (e) => {
     e.preventDefault();
     
-    var keys = Object.keys(openrct2.Platform);
+    const keys = Object.keys(openrct2.Platform);
 
-    for (var i = 0; i <= keys.length; i++) {
+    for (let i = 0; i <= keys.length; i++) {
       if (openrct2.Platform[keys[i]].name === platform.innerHTML) {
         if (i + 1 >= keys.length) {
           i = 1;
