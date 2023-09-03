910bd9829b7148237b40e6d1317e0cc0af9c5f6c
Michał Janiszewski <janisozaur+signed@gmail.com>
2023-09-03 21:50:18 +0200

    Add Windows Arm64 information

--- assets/js/openrct2.website.js
+++ assets/js/openrct2.website.js
@@ -2,6 +2,12 @@ var openrct2 = {};
 
 openrct2.Platform = Object.freeze({
     UNKNOWN: {},
+    WINDOWS_ARM64: {
+        name: 'Windows (ARM64)',
+        link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.4.6/OpenRCT2-0.4.6-windows-installer-arm64.exe',
+        size: 56647160,
+        version: '0.4.6'
+    },
     WINDOWS32: {
         name: 'Windows (32-bit)',
         link: 'https://github.com/OpenRCT2/OpenRCT2/releases/download/v0.4.6/OpenRCT2-0.4.6-windows-installer-win32.exe',
@@ -30,7 +36,9 @@ openrct2.Platform = Object.freeze({
 
 function getPlatform(){
     if (navigator.platform.indexOf('Win') >= 0){
-        if (navigator.userAgent.indexOf("WOW64") === -1 && navigator.userAgent.indexOf("Win64") === -1 ){
+        if (navigator.userAgent.indexOf("ARM64") >= 0 ){
+            return openrct2.Platform.WINDOWS_ARM64;
+        } else if (navigator.userAgent.indexOf("WOW64") === -1 && navigator.userAgent.indexOf("Win64") === -1 ){
             return openrct2.Platform.WINDOWS32;
         } else {
             return openrct2.Platform.WINDOWS64;  // 64-bit is the default as it is by far the most common these days
